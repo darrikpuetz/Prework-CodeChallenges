69f052243089333531429d7b7a5aeeeba09200f2
Darrik Puetz <darrikpuetz@gmail.com>
2019-10-13 20:13:57 -0700

    finished 1-3 challenges

---
+++ Solution1/ChallengeThree/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ChallengeThree
+{
+    class Program
+    {
+        static void Main(int[] perfectSequense)
+        {
+            Console.WriteLine("Array: " + String.Join(',', perfectSequense));
+            int sum = 0;
+            int product = 1;
+            foreach (var number in perfectSequense)
+            {
+                sum += number;
+                product *= number;
+            }
+            if (sum == product)
+            {
+                Console.WriteLine(String.Join(',', perfectSequense) + " is a perfect sequence");
+            }
+            else
+            {
+                Console.WriteLine(String.Join(',', perfectSequense) + " is not a perfect sequense");
+            }
+            Console.ReadLine();
+        }
+    }
+}
